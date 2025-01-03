af1d5ca8286bfe4ccf8274e2059bbba089b817f8
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-01-03 19:19:01 +0530

    Create q01.java

---
+++ q01.java
@@ -0,0 +1,9 @@
+//assignment 1 - java program to display your name and roll number
+public class assignment1
+{
+    public static void main( String args[] )
+    {
+        System.out.println("SAJJAD");
+        System.out.println("20221033");
+    }
+}
