997e29d4765bc8b6ee22da89a7791495287a6f62
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-01-03 19:29:14 +0530

    Create q05.java

---
+++ q05.java
@@ -0,0 +1,12 @@
+//assignment 5 - java program to display the values of a boolean variable
+public class assignment5
+{
+    public static void main( String args[] )
+    {
+        boolean b1, b2;
+        b1 = true;
+        b2 = false;
+        System.out.println("b1 is " + b1);
+        System.out.println("b2 is " + b2);
+    }
+}
