0702a055d91c5c93a023a0c46b73b0ea4cb74459
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-01-09 12:40:21 +0530

    q08

---
+++ q08.java
@@ -0,0 +1,13 @@
+//assignment 8 - java program to create an integer array, initialize it and print its values
+public class assignment8
+{
+    public static void main( String args[] )
+    {
+        int arr[] = {4,9,6,80,25,37,0};
+        System.out.print("The required array is ");
+        for(int i=0;i<7;i++){
+            
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
