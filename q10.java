8ab1409afb2f371df154a21ea96e16f652e43e28
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-01-09 12:41:39 +0530

    q10.java

---
+++ q10.java
@@ -0,0 +1,10 @@
+//assignment 10 - java program to show the use of ?: operator
+public class assignment10
+{
+    public static void main( String args[] )
+    {
+        int a = 10, b = 20;
+        int min = a<b ? a : b;
+        System.out.println("The smaller integer is " + min);
+    }
+}
