b67c5d07a57e1adb88b3c4b993b10cda932322e5
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-01-12 20:32:46 +0530

    Create q11.java

---
+++ q11.java
@@ -0,0 +1,15 @@
+//assignment 11 - java program to shoe if-else selection structure
+public class assignment11
+{
+    public static void main( String args[] )
+    {
+        int a = 10, b = 20, max;
+        if(a>b){
+            max = a;
+        }
+        else{
+            max = b;
+        }
+        System.out.println("The larger integer is " + max);
+    }
+}
