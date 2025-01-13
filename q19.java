355508ba4ece4b375e6f1a7a5213b431a5149618
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-01-13 17:46:35 +0530

    q19.java

---
+++ q19.java
@@ -0,0 +1,12 @@
+//assignment 19 - java program to show the use of return
+public class assignment19
+{
+    public static void main( String args[] )
+    {   
+        System.out.println("before the return");
+        if(true){
+            return;
+        }   
+        System.out.println("after the return, won't execute");
+    }
+}
