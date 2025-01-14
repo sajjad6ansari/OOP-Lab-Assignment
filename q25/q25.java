1633efdf6a85f6bc134e179d3a810e9e93fbcd8a
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-01-14 18:26:45 +0530

    q25.java

---
+++ q25/q25.java
@@ -0,0 +1,11 @@
+//assignment 25 - java program to show the use of private, public, protected and default access specifiers
+public class assignment25
+{
+    public static void main(String args[])
+    {
+        p1.assignment25a.printresults();
+        p1.assignment25b.printresults();
+        p2.assignment25c.printresults();
+        p2.assignment25d.printresults();
+    }
+}
