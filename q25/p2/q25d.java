dc52489d6740d148b7ed8514f703b72ac613ab6b
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-01-14 18:29:48 +0530

    q25d.java

---
+++ q25/p2/q25d.java
@@ -0,0 +1,9 @@
+package p2;
+public class assignment25d {
+    public static void printresults(){
+            System.out.println("for different package and different class");
+            System.out.println("every access method fails");
+            System.out.println("");
+    }
+    
+}
